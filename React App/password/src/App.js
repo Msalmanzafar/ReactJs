f6c04b27eab800674a14473d9ce70cfbbc88be13
msalmanzafar <m.salman.zafar292@gmail.com>
2017-02-15 13:35:17 +0500

    password project

---
+++ React App/password/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse ">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="{{JavaScript:void(0)}}">Password Validations Using ReactJs</a>
+        </div>
+      </div>
+    </nav>      
+    );
+  }
+}
+
+export default App;
