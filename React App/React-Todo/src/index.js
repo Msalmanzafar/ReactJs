2a56b05dde4adcea701e02796a196658f97964f1
msalmanzafar <m.salman.zafar292@gmail.com>
2017-02-21 12:40:11 +0500

    Firebase authentication

--- React App/React-Todo/src/index.js
+++ React App/React-Todo/src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import List from './list-layout';
+import { Route, Router, browserHistory } from 'react-router';
+import SignUp from './signup';
+import Signin from './signin.js';
+import TodoList from './todo-list';
+import * as firebase from 'firebase';
 
-import { Route, Router, browserHistory, IndexRoute} from 'react-router';
+
+// initialize firebase
+const config = {
+    apiKey: "AIzaSyArVpiyJ0wPxATriQBnPzvbCqjIBVWSppc",
+    authDomain: "sky-auth.firebaseapp.com",
+    databaseURL: "https://sky-auth.firebaseio.com",
+    storageBucket: "sky-auth.appspot.com",
+    messagingSenderId: "936918632809"
+};
+firebase.initializeApp(config);
+
 
 ReactDOM.render((
-  <Router history = {browserHistory}>
-      <Route path = "/" component = {App}>
-         {/*<IndexRoute component = {SignUp} />*/}
-      </Route>
-   </Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/todo-list" component={TodoList} />
+    </Route>
+  </Router>
 ), document.getElementById('root'));
 
 
--- React App/React-Todo/src/todo-list.js
+++ React App/React-Todo/src/todo-list.js
@@ -1,24 +1,26 @@
-import _ from 'lodash';
-import React,{Component} from 'react';
-import TodoListHead from './todo-list-head';
-import TodoListItem from './todo-list-item';
+import React, { Component } from 'react';
 
-
-class TodoList extends Component {
-    
-    renderItems(){
-        return _.map(this.props.todos, (todo, index) => <TodoListItem key={index
-        }{...todo}/>);
-    }
-
-    render(){        
+class TodoList extends Component{
+    render(){
         return(
-            <table className="table table-bordered table">
-                <TodoListHead  />   
-                <tbody>
-                    {this.renderItems()}
-                </tbody>               
-            </table>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10 col-md-offset-2">
+                        <blockquote>
+                            <form>
+                                <div className="form-group">
+                                    <div className="col-md-8">
+                                        <input type="text" className="form-control" placeholder="Enter your message.."/>
+                                    </div>
+                                    <div className="col-md-2 ">
+                                        <button type="button" className="btn btn-success">Send</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
